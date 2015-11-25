4b48bae1c5bb9655a941a2a99409c07f417a0155
agustinpfs <agustinpfs@gmail.com>
2015-11-25 10:06:33 -0300

    same example with conditional err

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
 fs.readFile('./public/index.html', function (err, data) {
+	if (err) {
+		return console.log('no se pudo abrir: ' + err.message)
+	}
 	console.log(data.toString().length)
 })
 
