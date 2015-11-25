7f22c4efda285b8392b1cb68c9ebec43865aecd2
agustinpfs <agustinpfs@gmail.com>
2015-11-25 12:17:02 -0300

    create module assets.js- use arrow function example

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 var fs = require('fs')
 var http = require('http')
+var assets = require('./assets.js')
 
-function serveStatic (name, callback) {
-  fs.readFile('./public/' + name, function (err, data) {
-    if (err) {
-      return callback(err)
-    }
-    callback(err, data.toString())
-  })
-}
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer((request, response) => {
   console.log('Recibi un request ' + request.url)
   switch (request.url) {
     case '/':
-      serveStatic('index.html', function (err, content) {
+      assets.serveStatic('index.html', function (err, content) {
         response.end(content)
       })
       break
     case '/app.js':
-      serveStatic('app.js', function (err, content) {
+      assets.serveStatic('app.js', function (err, content) {
         response.end(content)
       })
       break
     case '/app.css':
-      serveStatic('app.css', function (err, content) {
+      assets.serveStatic('app.css', function (err, content) {
         response.end(content)
       })
       break
