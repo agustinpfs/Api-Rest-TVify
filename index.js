3b017a3c521cabef33711d74bdcb84fbc4b36f25
agustinpfs <agustinpfs@gmail.com>
2015-11-25 11:18:44 -0300

    create new web server

--- index.js
+++ index.js
@@ -11,9 +11,37 @@ fs.readFile('./public/index.html', function (err, data) {
 console.log('hola mundo')
 
 var server = http.createServer(function (request,response){ //each time it runs a request
-	console.log('recibí un request ' + request-url)
-	response.writeHead('Content-Type', 'text/plain')
-	response.end('hola mundo')
+	console.log('recibí un request ' + request.url)
+	 switch (request.url) {
+	    case '/':
+	     fs.readFile('./public/index.html', function (err, data) {
+			if (err) {
+				return console.log('no se pudo abrir: ' + err.message)
+			}
+			response.end(data.toString())
+	      })
+	      break
+	    case '/app.js':
+	      fs.readFile('./public/app.js', function (err, data) {
+			if (err) {
+				return console.log('no se pudo abrir: ' + err.message)
+			}
+			response.end(data.toString())
+	      })
+	      break
+	    case '/app.css':
+	      fs.readFile('./public/app.css', function (err, data) {
+			if (err) {
+				return console.log('no se pudo abrir: ' + err.message)
+			}
+			response.end(data.toString())
+	      })
+	      break
+      default:
+      	response.end('')
+      	break
+	  }
+	
 })
 
 server.listen(3000, function(){
