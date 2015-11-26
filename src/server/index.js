07fc7d578d71f73126d610a9825df9534c9cd066
agustinpfs <agustinpfs@gmail.com>
2015-11-26 19:43:34 -0300

    add votes count

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,28 @@
 import express from 'express'
 const app = express() // const: la referencia no va a cambiar nunca
 
+//object (DB)
+const votes = {}
+
+
 app.use(express.static('public'))  // middleware. each time enter a request, it will run this middelware
 
 
 //endpoints
-// GET
+// GET /votes
 app.get('/votes', (req, res) => { // all request entering with GET method and /votes url, it will be captured this function and will run this callback
-  res.json([])
+  res.json(votes)
 })
 
 // POST /vote/<id>
 app.post('/vote/:id', (req, res) =>{ // all request entering with this function
-
+  let id = req.params.id
+  if (votes[id] === undefined){
+    votes[id] = 1
+  } else {
+    votes[id] = votes[id] + 1 //add 1 vote
+  }
+  res.json({votes: votes[id]})
 })
 
 app.listen(3000, () => console.log('Servidor iniciado. Escuchando en el puerto 3000'))
