317cfa2e0872e6300402700f922f6b33deb44caf
agustinpfs <agustinpfs@gmail.com>
2015-11-26 09:31:36 -0300

    workshop create function asynchronous whit events - publish subscribe

--- taller.js
+++ taller.js
@@ -1,16 +1,44 @@
-// create function asynchronous whithout readFile
+// create events
 
 var fs = require('fs')
+var EventEmitter = require('events')
+var util = require('util')
+var inherits = util.inherits
 
 function readFileText (name, callback) {
-	process.nextTick(function(){  //agarra mi funcion y la pone en cola de eventos
-		var content = fs.readFileSync(name)// hasta que el eventloop decida q termino y llame al callback
+	process.nextTick(function(){  
+		var content = fs.readFileSync(name)
 		callback(content.toString())							
 	})					   
 }
 
-readFileText('./lorem.txt', function(content){
-	console.log(content)	
+// create class
+function TextReader (name){
+	EventEmitter.call(this)
+	this.name = name
+}
+
+inherits(TextReader, EventEmitter)
+
+TextReader.prototype.read = function () {
+  var self = this
+  readFileText(this.name, function (content) {
+    self.emit('end', content)
+  })
+}
+
+// readFileText('./lorem.txt', function(content){
+// 	console.log(content)	
+// })
+
+
+var reader = new TextReader('./lorem.txt')
+
+reader.on('end', function(content){
+	console.log(content)
 })
+reader.read()
 
 console.log('hola mundo')
+
+
