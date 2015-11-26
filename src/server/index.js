24bcf533081ff3ca5c2b9e945352d5bc9cfcacae
agustinpfs <agustinpfs@gmail.com>
2015-11-26 19:00:03 -0300

    run with babel

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,21 @@
-var express = require('express')
-var app = express()
+import express from 'express'
+const app = express() // const: la referencia no va a cambiar nunca
 
 app.use(express.static('public'))  // middleware. each time enter a request, it will run this middelware
 
 
 //endpoints
 // GET
-app.get('/votes', function(req, res){ // all request entering with GET method and /votes url, it will be captured this function and will run this callback
+app.get('/votes', (req, res) => { // all request entering with GET method and /votes url, it will be captured this function and will run this callback
   res.json([])
 })
 
 // POST /vote/<id>
-app.post('/vote/:id',function(req, res){ // all request entering with this function
+app.post('/vote/:id', (req, res) =>{ // all request entering with this function
 
 })
 
-app.listen(3000, function (){
-  console.log('Servidor iniciado. Escuchando en el puerto 3000')
-})
+app.listen(3000, () => console.log('Servidor iniciado. Escuchando en el puerto 3000'))
+
+
+// RUN: babel-node src/server --presets es2015
