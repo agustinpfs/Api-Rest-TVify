efa18d9a6d15275bf7a96e76b9df4e2ed4197ad6
agustinpfs <agustinpfs@gmail.com>
2015-11-26 10:01:20 -0300

    same example with 2 subscribers

--- taller.js
+++ taller.js
@@ -1,44 +1,5 @@
-// create events
+var contenido = require('./content')
+var caracteres = require('./caracters')
+var reader = require('./reader')
 
-var fs = require('fs')
-var EventEmitter = require('events')
-var util = require('util')
-var inherits = util.inherits
-
-function readFileText (name, callback) {
-	process.nextTick(function(){  
-		var content = fs.readFileSync(name)
-		callback(content.toString())							
-	})					   
-}
-
-// create class
-function TextReader (name){
-	EventEmitter.call(this)
-	this.name = name
-}
-
-inherits(TextReader, EventEmitter)
-
-TextReader.prototype.read = function () {
-  var self = this
-  readFileText(this.name, function (content) {
-    self.emit('end', content)
-  })
-}
-
-// readFileText('./lorem.txt', function(content){
-// 	console.log(content)	
-// })
-
-
-var reader = new TextReader('./lorem.txt')
-
-reader.on('end', function(content){
-	console.log(content)
-})
 reader.read()
-
-console.log('hola mundo')
-
-
