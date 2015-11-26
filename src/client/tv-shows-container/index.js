08198075de2fb26b9a57bc15fabb0455d1f7e24b
agustinpfs <agustinpfs@gmail.com>
2015-11-26 20:11:00 -0300

    get id +1

--- src/client/tv-shows-container/index.js
+++ src/client/tv-shows-container/index.js
@@ -4,7 +4,10 @@ var $tvShowsContainer = $('#app-body').find('.tv-shows')
 
 $tvShowsContainer.on('click', 'button.like', function (ev) {
   var $this = $(this);
-  $this.closest('.tv-show').toggleClass('liked')
+  var id = $this.data('id') // return data-id(index.js-render)
+  $.post('/vote/' + id, function(){  //ajax jquery
+  	$this.closest('.tv-show').toggleClass('liked')
+  })  
 })
 
 export default $tvShowsContainer
