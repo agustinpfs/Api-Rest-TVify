c925dd663373b41c1aa229b4c004ce98c0e65c45
agustinpfs <agustinpfs@gmail.com>
2015-11-26 11:16:37 -0300

    delete files workshop

--- caracters.js
+++
@@ -1,6 +0,0 @@
-// subscriber
-var reader = require('./reader')
-
-reader.on('end', function (content) {
-  console.log(content.length)
-})
--- taller.js
+++
@@ -1,5 +0,0 @@
-var contenido = require('./content')
-var caracteres = require('./caracters')
-var reader = require('./reader')
-
-reader.read()
