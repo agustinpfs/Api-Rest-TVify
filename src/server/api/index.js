03bc1857203b8bccbc96a55a5f952b8b4a9417c4
agustinpfs <agustinpfs@gmail.com>
2015-11-27 13:00:34 -0300

    routes express

---
+++ src/server/api/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+const router = express.Router() 
+
+const votes = {}
+
+
+//endpoints
+// GET /api/votes
+router.get('/votes', (req, res) => { // all request entering with GET method and /votes url, it will be captured this function and will run this callback
+  res.json(votes)
+})
+
+// POST /api/vote/<id>
+router.post('/vote/:id', (req, res) =>{ // all request entering with this function
+  let id = req.params.id
+  if (votes[id] === undefined){
+    votes[id] = 1
+  } else {
+    votes[id] = votes[id] + 1 //add 1 vote
+  }
+  res.json({votes: votes[id]})
+})
+
+export default router
--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,12 @@
 import express from 'express'
 const app = express() // const: la referencia no va a cambiar nunca
 
-//object (DB)
-const votes = {}
+import api from 'src/server/api'
 
 
 app.use(express.static('public'))  // middleware. each time enter a request, it will run this middelware
 
-
-//endpoints
-// GET /votes
-app.get('/votes', (req, res) => { // all request entering with GET method and /votes url, it will be captured this function and will run this callback
-  res.json(votes)
-})
-
-// POST /vote/<id>
-app.post('/vote/:id', (req, res) =>{ // all request entering with this function
-  let id = req.params.id
-  if (votes[id] === undefined){
-    votes[id] = 1
-  } else {
-    votes[id] = votes[id] + 1 //add 1 vote
-  }
-  res.json({votes: votes[id]})
-})
+app.use('/api', api)
 
 app.listen(3000, () => console.log('Servidor iniciado. Escuchando en el puerto 3000'))
 
